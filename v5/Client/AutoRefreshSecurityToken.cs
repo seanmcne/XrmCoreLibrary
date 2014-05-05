8517033e1ba7f09ec539afd7fde1e37eefcf07ba
Austin Jones <maustinjones@users.noreply.github.com>
2014-05-05 18:09:12 +0000

    v5.1404 release migrate to CodePlex.

---
+++ v5/Client/AutoRefreshSecurityToken.cs
@@ -0,0 +1,102 @@
+﻿// =====================================================================
+//  This file is part of the Microsoft Dynamics CRM SDK code samples.
+//
+//  Copyright (C) Microsoft Corporation.  All rights reserved.
+//
+//  This source code is intended only as a supplement to Microsoft
+//  Development Tools and/or on-line documentation.  See these other
+//  materials for detailed information regarding Microsoft code samples.
+//
+//  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+//  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//  PARTICULAR PURPOSE.
+// =====================================================================
+namespace Microsoft.Pfe.Xrm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.ServiceModel.Description;
+    using System.Text;
+
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Client;    
+
+    /// <summary>
+    /// Class that handles renewing the SecurityTokenResponse if expired
+    /// </summary>
+    public sealed class AutoRefreshSecurityToken<TProxy, TService>
+        where TProxy : ServiceProxy<TService>
+        where TService : class
+    {        
+        private TProxy _proxy;
+
+        /// <summary>
+        /// Instantiates an instance of the proxy class
+        /// </summary>
+        /// <param name="proxy">Proxy that will be used to authenticate the user</param>
+        public AutoRefreshSecurityToken(TProxy proxy)
+        {
+            if (null == proxy)
+            {
+                throw new ArgumentNullException("proxy");
+            }
+
+            this._proxy = proxy;
+        }
+
+        /// <summary>
+        /// Prepares authentication before authen6ticated
+        /// </summary>
+        public void PrepareCredentials()
+        {
+            if (null == this._proxy.ClientCredentials)
+            {
+                return;
+            }
+
+            switch (this._proxy.ServiceConfiguration.AuthenticationType)
+            {
+                case AuthenticationProviderType.ActiveDirectory:
+                    this._proxy.ClientCredentials.UserName.UserName = null;
+                    this._proxy.ClientCredentials.UserName.Password = null;
+                    break;
+                case AuthenticationProviderType.Federation:
+                case AuthenticationProviderType.OnlineFederation:
+                case AuthenticationProviderType.LiveId:
+                    this._proxy.ClientCredentials.Windows.ClientCredential = null;
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// Renews the token (if it is near expiration or has expired)
+        /// </summary>
+        public void RenewTokenIfRequired()
+        {
+            if (null != this._proxy.SecurityTokenResponse &&
+                DateTime.UtcNow.AddMinutes(15) >= this._proxy.SecurityTokenResponse.Response.Lifetime.Expires)
+            {
+                try
+                {
+                    this._proxy.Authenticate();
+                }
+                catch (CommunicationException)
+                {
+                    if (null == this._proxy.SecurityTokenResponse ||
+                        DateTime.UtcNow >= this._proxy.SecurityTokenResponse.Response.Lifetime.Expires)
+                    {
+                        throw;
+                    }
+
+                    // Ignore the exception 
+                }
+            }
+        }
+    }
+}
