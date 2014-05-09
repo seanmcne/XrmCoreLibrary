81423ab709217ce6c7de19982697618b50bc1e3f
Austin Jones <maustinjones@users.noreply.github.com>
2014-05-09 19:00:22 +0000

    fix assembly version number (6.0.1405.2)

--- v6/Properties/AssemblyInfo.cs
+++ v6/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1405.2")]
-[assembly: AssemblyFileVersion("6.1405.2")]
-[assembly: AssemblyInformationalVersion("6.1405.2")]
+[assembly: AssemblyVersion("6.0.1405.2")]
+[assembly: AssemblyFileVersion("6.0.1405.2")]
+[assembly: AssemblyInformationalVersion("6.0.1405.2")]
