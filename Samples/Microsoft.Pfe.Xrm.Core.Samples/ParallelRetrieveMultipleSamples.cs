7bc52033aecf68e54e127c7b14300009b5cc420a
maustinjones_cp <SND\maustinjones_cp>
2014-09-09 21:21:11 +0000

    update samples to demonstrate 1409 features.

--- Samples/Microsoft.Pfe.Xrm.Core.Samples/ParallelRetrieveMultipleSamples.cs
+++ Samples/Microsoft.Pfe.Xrm.Core.Samples/ParallelRetrieveMultipleSamples.cs
@@ -41,25 +41,28 @@ namespace Microsoft.Pfe.Xrm.Samples
         /// <summary>
         /// Demonstrates parallelized submission of multiple queries
         /// </summary>
-        /// <param name="queries">The list of queries to retrieve</param>
-        /// <returns>The list of query result collections</returns>
-        public List<EntityCollection> ParallelRetrieveMultiple(List<QueryBase> queries)
+        /// <param name="queries">The keyed collection of queries to retrieve</param>
+        /// <returns>The keyed collection of query result collections</returns>
+        public IDictionary<string, EntityCollection> ParallelRetrieveMultiple(IDictionary<string, QueryBase> queries)
         {
-            List<EntityCollection> results = null;
+            IDictionary<string, EntityCollection> results = null;
             
             try
             {
-                results = this.Manager.ParallelProxy.RetrieveMultiple(queries).ToList();
+                results = this.Manager.ParallelProxy.RetrieveMultiple(queries);
+
+                foreach (var result in results)
+                {
+                    Console.WriteLine("Query with key={0} for {1} returned {2} records.", 
+                        result.Key, 
+                        result.Value.EntityName, 
+                        result.Value.Entities.Count);
+                }
             }
             catch (AggregateException ae)
             {
                 // Handle exceptions
             }
-
-            results.ForEach(r =>
-                {
-                    Console.WriteLine("Query of {0} returned {1} records.", r.EntityName, r.Entities.Count);
-                });
 
             return results;
         }
@@ -67,25 +70,28 @@ namespace Microsoft.Pfe.Xrm.Samples
         /// <summary>
         /// Demonstrates parallelized submission of multiple queries where all pages of results are retrieved for each query
         /// </summary>
-        /// <param name="queries">The list of queries to retrieve</param>
-        /// <returns>The list of query result collections</returns>
-        public List<EntityCollection> ParallelRetrieveMultipleAllPages(List<QueryBase> queries)
+        /// <param name="queries">The keyed collection of queries to retrieve</param>
+        /// <returns>The keyed collection of query result collections</returns>
+        public IDictionary<string, EntityCollection> ParallelRetrieveMultipleAllPages(IDictionary<string, QueryBase> queries)
         {
-            List<EntityCollection> results = null;
+            IDictionary<string, EntityCollection> results = null;
 
             try
             {
-                results = this.Manager.ParallelProxy.RetrieveMultiple(queries, true).ToList();
+                results = this.Manager.ParallelProxy.RetrieveMultiple(queries, true);
+
+                foreach (var result in results)
+                {
+                    Console.WriteLine("Query with key={0} for {1} returned {2} records.",
+                        result.Key,
+                        result.Value.EntityName,
+                        result.Value.Entities.Count);
+                }
             }
             catch (AggregateException ae)
             {
                 // Handle exceptions
             }
-
-            results.ForEach(r =>
-            {
-                Console.WriteLine("Query of {0} returned {1} records.", r.EntityName, r.Entities.Count);
-            });
 
             return results;
         }
@@ -93,23 +99,23 @@ namespace Microsoft.Pfe.Xrm.Samples
         /// <summary>
         /// Demonstrates parallelized submission of multiple queries. Queries can be mix of QueryExpression and FetchXML
         /// </summary>
-        /// <returns>The list of query result collections</returns>
-        public List<EntityCollection> ParallelRetrieveMultipleFull()
+        /// <returns>The keyed collection of query result collections</returns>
+        public IDictionary<string, EntityCollection> ParallelRetrieveMultipleFull()
         {
-            List<EntityCollection> results = null;
-            var queries = new List<QueryBase>();
+            IDictionary<string, EntityCollection> results = null;
+            var queries = new Dictionary<string, QueryBase>();
 
             var accountQuery = new QueryExpression("account");
             accountQuery.ColumnSet.AddColumns("name", "address1_city", "primarycontactid");
             accountQuery.Criteria.AddCondition(new ConditionExpression("name", ConditionOperator.BeginsWith, "C"));
             accountQuery.NoLock = true;
-            queries.Add(accountQuery);
+            queries.Add("accounts", accountQuery);
 
             var contactQuery = new QueryExpression("contact");
             contactQuery.ColumnSet.AddColumns("firstname", "lastname", "parentcustomerid");
             contactQuery.Criteria.AddCondition(new ConditionExpression("statecode", ConditionOperator.Equal, 1));
             contactQuery.NoLock = true;
-            queries.Add(contactQuery);
+            queries.Add("contacts", contactQuery);
 
             var oppQuery = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
                                 <entity name='opportunity'>
@@ -121,21 +127,24 @@ namespace Microsoft.Pfe.Xrm.Samples
                                     </filter>
                                 </entity> 
                                 </fetch>";
-            queries.Add(new FetchExpression(oppQuery));
+            queries.Add("opportunities", new FetchExpression(oppQuery));
 
             try
             {
-                results = this.Manager.ParallelProxy.RetrieveMultiple(queries, true).ToList();
+                results = this.Manager.ParallelProxy.RetrieveMultiple(queries, true);
+
+                foreach (var result in results)
+                {
+                    Console.WriteLine("Query with key={0} for {1} returned {2} records.",
+                        result.Key,
+                        result.Value.EntityName,
+                        result.Value.Entities.Count);
+                }
             }
             catch (AggregateException ae)
             {
                 // Handle exceptions
             }
-
-            results.ForEach(r =>
-            {
-                Console.WriteLine("Query of {0} returned {1} records.", r.EntityName, r.Entities.Count);
-            });
 
             return results;
         } 
