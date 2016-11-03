d627ffe26c7b0ff2ad91bdcf484db015067730c1
Austin Jones <maustinjones@users.noreply.github.com>
2016-11-03 22:30:22 +0000

    fix bachextensions OOM issue and add max records call arg to retrieve multiple extension

---
+++ v8/Properties/AssemblyVersionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("8.1.1611.0301")]
+[assembly: AssemblyFileVersion("8.1.1611.0301")]
+[assembly: AssemblyInformationalVersion("8.1.1611.0301")]
