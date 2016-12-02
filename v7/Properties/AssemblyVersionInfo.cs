5f9cce52fe915c92c7d26d825453ed8b0911d585
Austin Jones <maustinjones@users.noreply.github.com>
2016-12-02 01:55:32 +0000

    1612.1 release changes

--- v7/Properties/AssemblyVersionInfo.cs
+++ v7/Properties/AssemblyVersionInfo.cs
@@ -10,6 +10,6 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.1611.0301")]
-[assembly: AssemblyFileVersion("7.0.1611.0301")]
-[assembly: AssemblyInformationalVersion("7.0.1611.0301")]
+[assembly: AssemblyVersion("7.0.1612.0101")]
+[assembly: AssemblyFileVersion("7.0.1612.0101")]
+[assembly: AssemblyInformationalVersion("7.0.1612.0101")]
